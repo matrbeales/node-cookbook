d2ff399c44bb89cc7303817ca6f9e8bd60e52dda
unknown <mbeales@spartaglobal.com>
2019-02-06 16:33:34 +0000

    node cookbook created

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+include_recipe "apt"
+include_recipe "nodejs"
+
+nodejs_npm "pm2"
+package "nginx"
+
+service "nginx" do
+  action [:enable, :start]
+end
+
+template "/etc/nginx/nginx.conf" do
+  source "nginx.conf.erb"
+  variables proxy_port: node["nginx"]["proxy_port"]
+  notifies :restart, "service[nginx]"
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -11,25 +11,37 @@ describe 'node::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
+    it "should install nginx" do
+      expect(chef_run).to install_package("nginx")
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it "should enable ngninx as a service" do
+      expect(chef_run).to enable_service("nginx")
     end
+
+    it "should start ngninx as a service" do
+      expect(chef_run).to start_service("nginx")
+    end
+
+    it "should install nodejs from a recipe" do
+      expect(chef_run).to include_recipe("nodejs")
+    end
+
+    it "should install pm2 via npm" do
+      expect(chef_run).to install_nodejs_npm("pm2")
+    end
+
+    it "should create a nginx.conf template in /etc/nginx" do
+      expect(chef_run).to create_template("/etc/nginx/nginx.conf").with_variables(proxy_port: 3000)
+    end
+
   end
 end
